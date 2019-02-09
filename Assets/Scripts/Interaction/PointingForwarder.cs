4adc65fbd8be13c58bdda95850cb29cc853354eb
yutoVR <yuto.fushimi@icloud.com>
2019-02-09 12:37:42 +0900

    Pointing Signal Forwarder

---
+++ Assets/Scripts/Interaction/PointingForwarder.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PointingForwarder : MonoBehaviour, IPointable
+{
+	[SerializeField] MonoBehaviour target;
+	IPointable pointable;
+
+	void Awake()
+	{
+		pointable = target as IPointable;
+		if (pointable == null)
+		{
+			Debug.LogError("target が IPointable ではありません。対象を確認して下さい。");
+			Destroy(this);
+		}
+	}
+
+	public void Pointed()
+	{
+		pointable.Pointed();
+	}
+
+	public void UnPointed()
+	{
+		pointable.UnPointed();
+	}
+
+	public void Click()
+	{
+		pointable.Click();
+	}
+}
