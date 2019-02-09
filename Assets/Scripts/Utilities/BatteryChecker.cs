21098abd57c0bfd4cdf9498d3567af5896eb25df
yutoVR <yuto.fushimi@icloud.com>
2019-02-09 14:41:23 +0900

    Fixed a bug that prevent playing after first session.

--- Assets/Scripts/Utilities/BatteryChecker.cs
+++ Assets/Scripts/Utilities/BatteryChecker.cs
@@ -4,14 +4,22 @@ public class BatteryChecker : MonoBehaviour
 {
 	[SerializeField, Range(0f, 100f), Header("警告を表示するバッテリ残量（%）")] float threshold = 10f;
 
+	void OnEnable()
+	{
+		PlaybackController.onStartPlaying += Hide;
+	}
+
+	void OnDisable()
+	{
+		PlaybackController.onStartPlaying -= Hide;
+	}
+
 	void Start()
 	{
 		// バッテリ残量が取得できない場合
 		if (SystemInfo.batteryLevel == -1f) Hide();
 
 		if (SystemInfo.batteryLevel > threshold / 100f) Hide();
-
-		PlaybackController.onStartPlaying += Hide;
 	}
 
 	void Hide()
